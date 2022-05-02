ba074d91cc277a40e26e6d2ba94b99c9475091d5
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 16:10:57 -0700

    employee info added

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,13 @@ VALUES ("Accountant", 90000, 1),
        ("Sales Manager", 80000, 5),
        ("Sales Associate", 50000, 5),
        ("Customer Representative", 40000, 6);
+
+       
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 5, 5),
+       ("Mike", "Gar", 6, null),
+       ("Tom", "Thompson", 7, 2),
+       ("Su", "Fly", 2, null),
+       ("James", "Char", 4, null),
+       ("Robert", "Downey", 5, 5),
+       ("Bill", "Burt", 3, 4);
