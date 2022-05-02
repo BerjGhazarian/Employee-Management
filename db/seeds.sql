e6e415bdf83e543d5ab03faa6c326092f7d3b91e
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 16:08:39 -0700

    added title salary and department id

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,13 @@ VALUES ("Accounting"),
        ("Legal"),
        ("Sales"),
        ("Service");
+
+INSERT INTO employee_role(title, salary, department_id)
+VALUES ("Accountant", 90000, 1),
+       ("Lead Engineer", 130000, 2),
+       ("Web Developer", 80000, 2),
+       ("Legal Lead", 120000, 4),
+       ("Legal Advisor", 100000, 4),
+       ("Sales Manager", 80000, 5),
+       ("Sales Associate", 50000, 5),
+       ("Customer Representative", 40000, 6);
