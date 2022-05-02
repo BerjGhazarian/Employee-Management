8b924a9b9c636b7628878fe06ee5cb5c40e17dd3
berj ghazarian <ghazarian_berj@hotmail.com>
2022-05-02 16:13:18 -0700

    main menu added

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee database.`)
   );
+
+  function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "viewOptions",
+          message: "What would you like to do?",
+          choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee Role",
+            "I'm Done",
+          ],
+        },
+      ])
